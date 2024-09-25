568bc73019a587097344674debd78ac9cfbd1029
kobyrthr-dev <kobyrthr@gmail.com>
2024-09-24 21:10:14 -0500

    nominal cat_vars

--- Summary_Statistics_for_Categorical_Data.py
+++ Summary_Statistics_for_Categorical_Data.py
@@ -9,3 +9,19 @@ print(nyc_trees.head)
 # Which columns are categorical vars?
 categorical_vars = ["neighborhood","status","health","spc_common"]
 
+# NOMINAL CATEGORIES
+import pandas as pd
+
+# Read NYC Trees Data
+nyc_trees = pd.read_csv("./nyc_tree_census.csv")
+
+# Get tree counts by neighborhood
+tree_counts=nyc_trees["neighborhood"].value_counts()
+
+print(tree_counts)
+
+# Get neighborhoods with most trees
+greenest_neighborhood = (tree_counts.index[0])
+
+print(greenest_neighborhood)
+
