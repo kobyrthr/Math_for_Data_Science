9923cea67722031c87b00389d39443583d82c996
kobyrthr-dev <kobyrthr@gmail.com>
2024-10-15 06:48:59 -0500

    poisson, lambda with large numbers

--- poisson_intro.py
+++ poisson_intro.py
@@ -1,4 +1,7 @@
 import scipy.stats as stats
+import codecademylib3
+
+from histogram_function import histogram_function
 
 ## Checkpoint 1
 # calculate prob_15
@@ -24,3 +27,17 @@ print (prob_more_than_20)
 prob_17_to_21 = stats.poisson.cdf(21,15) - stats.poisson.cdf(16,15)
 
 print (prob_17_to_21)
+
+## Checkpoint 1
+# lambda = 15, 1000 random draws 
+rand_vars = stats.poisson.rvs(15, size = 1000)
+
+
+## Checkpoint 2
+# print the mean of rand_vars
+print(rand_vars.mean())
+
+
+## Checkpoint 3
+histogram_function(rand_vars)
+
