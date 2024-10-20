c580447e5f8eab455af92640793cf390e7dbeb0c
kobyrthr-dev <kobyrthr@gmail.com>
2024-10-20 09:38:16 -0500

    numpy poisson and variance

--- Probability/Probablity.py
+++ Probability/Probablity.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats as stats
 
 # create 6 sided "die"
 die_6 = range(1, 7)
@@ -16,3 +17,20 @@ die_12 = range(1,13)
 # roll the 12-sided "die" 10 times
 results_2 = np.random.choice(die_12, size=10, replace=True)
 
+# PROBABILTY MASS FUNCTION WITH BINOIAL DISTRIBUTION
+
+ value of interest
+# change this
+x = 3
+
+# sample size
+# change this
+n = 10
+
+# calculate probability
+prob_1 = stats.binom.pmf(x, n, 0.5)
+print(prob_1)
+
+## Question 2
+prob_2 = stats.binom.pmf(7,20, .5)
+print(prob_2)
